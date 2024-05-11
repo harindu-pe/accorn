788525aa72cd1a2694f5a952cd70141d8339e831
Harindu Egalla <harindu.e@asyncmetric.com>
2024-05-12 03:54:53 +0530

    header done

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/global/header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <main className="px-3 lg:px-14">
+        {children}
+        hello layout
+      </main>
+    </>
+  );
+}
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <p>This is an authenticated route.</p>
       <UserButton afterSignOutUrl="/" />
-    </main>
+    </div>
   );
 }
